4e4f837a8519001895c902e5e5b32768e190e01a
oo009pbh <oo009pbh@gmail.com>
2023-01-13 13:16:49 +0900

    [1월 2주차] 줄 서는 방법

--- ParkByoengHun/1월 2주차/줄 서는 방법.js
+++ ParkByoengHun/1월 2주차/줄 서는 방법.js
@@ -1,18 +1,23 @@
 function solution(n, k) {
     let answer = [];
     let array = Array.from({length: n}, (v, i) => i + 1);
+    let nFacto = factorial(n);
 
+    while (answer.length < n) {
+        nFacto = nFacto / array.length;
+        answer.push(...array.splice(Math.floor((k - 1) / nFacto), 1));
+        k = k % nFacto;
+    }
 
     return answer;
 }
 
 const factorial = (n) => {
     let answer = 1;
-    for (let i = 1; i < n; i ++) {
+    for (let i = 1; i <= n; i ++) {
         answer *= i;
     }
     return answer;
 }
-//아직 안푼 상태
 
 console.log(solution(3, 5))
